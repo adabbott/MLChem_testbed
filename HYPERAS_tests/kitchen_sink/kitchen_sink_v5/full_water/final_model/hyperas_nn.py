94f3be18a61b237aaceba78545b31119a69f44a5
adabbott <adabbott@uga.edu>
2018-09-05 18:54:07 -0400

    added robust hp opt for water pes

---
+++ HYPERAS_tests/kitchen_sink/kitchen_sink_v5/full_water/final_model/hyperas_nn.py
@@ -0,0 +1,149 @@
+from hyperopt import Trials, STATUS_OK, tpe
+from hyperas import optim
+from hyperas.distributions import choice, uniform
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation
+from keras.models import load_model
+from keras.optimizers import Adam
+from keras.callbacks import EarlyStopping
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import QuantileTransformer
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold 
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
+
+import pandas as pd
+import numpy
+numpy.set_printoptions(threshold=10000, linewidth=150)
+
+
+# to be used later
+
+def data():
+    data = pd.read_csv("PES.dat") 
+    # drop duplicates, toggle on or off
+    #data = data.drop_duplicates(subset = 'E')
+    data = data.values
+    X = data[:,0:-1]
+    y = data[:,-1].reshape(-1,1)
+    return X,y
+
+# Standard scaling, 3 Fold CV, up to 5 layers, standard Adam optimizer default settings, 500 epochs. 
+# This HyperOpt is for neural architecture search, while optimizer parameters (lr, decay, amsgrad) will be searched across later.
+def create_model(X,y):
+    scaler = StandardScaler()
+    sX = scaler.fit_transform(X)
+    sy = scaler.fit_transform(y)
+
+    in_dim = tuple([sX.shape[1]])
+    out_dim = sy.shape[1]
+    kfold = KFold(n_splits=3, shuffle=True, random_state=42)
+    cv_losses = []
+    cv_maes = []
+    fcv_losses = []
+    fcv_maes = []
+    for train, test in kfold.split(sX, sy):
+        model = Sequential()
+        model.add(Dense(100, input_shape=in_dim))
+        model.add(Activation('tanh'))
+        model.add(Dense(50))
+        model.add(Activation('sigmoid'))
+        model.add(Dense(out_dim))
+        model.add(Activation('linear'))
+
+        opt = Adam(lr=0.0209407,
+                   beta_1=0.9,
+                   beta_2=0.999,
+                   epsilon=None,
+                   decay=0.0000001,
+                   amsgrad=True)
+
+        # rather than fix epochs, let it go until the model stops improving. 
+        # In scaled data, it is unclear what loss corresponds to 1 wavenumber (4.5e-6 hartrees) in scaled data. emprical guess of 1.0e-7.
+        callbacks = [EarlyStopping(monitor='loss', min_delta=5.0e-9, patience=5000)]
+        model.compile(loss='mse', optimizer=opt, metrics=['mae'])
+        model.fit(x=sX,y=sy,epochs=50000,batch_size=10,verbose=1, callbacks=callbacks)
+
+
+        # since comparing scaling methods, track model performance wrt actual energies
+        predicted_scaled_y = model.predict(sX[test])
+        predicted_y = scaler.inverse_transform(predicted_scaled_y.reshape(-1,1))
+        actual_y = scaler.inverse_transform(sy[test])
+        mae = mean_absolute_error(actual_y, predicted_y)
+        loss = mean_squared_error(actual_y, predicted_y)
+        #loss, mae = model.evaluate(sX[test], sy[test],verbose=0)
+        cv_losses.append(loss)
+        cv_maes.append(mae)
+
+        predicted_scaled_y = model.predict(sX)
+        predicted_y = scaler.inverse_transform(predicted_scaled_y.reshape(-1,1))
+        actual_y = scaler.inverse_transform(sy)
+        mae = mean_absolute_error(actual_y, predicted_y)
+        loss = mean_squared_error(actual_y, predicted_y)
+        fcv_losses.append(loss)
+        fcv_maes.append(mae)
+    print("Test MSEs: ", cv_losses)
+    print("Test MAEs: ", cv_maes)
+    print("Full MSEs: ", fcv_losses)
+    print("Full MAEs: ", fcv_maes)
+    loss = numpy.mean(cv_losses)
+    # this model is just the last model in CV, not necessarily the best
+    return {'loss': loss, 'status': STATUS_OK, 'model':model}
+
+#if __name__ == '__main__':
+#    best_run, best_model = optim.minimize(model=create_model,
+#                                          data=data,
+#                                          algo=tpe.suggest,
+#                                          max_evals=50,
+#                                          trials=Trials(),
+#                                          eval_space=True)
+#    with open("RESULTS.txt", "w+") as f:
+#        f.write("Best performing model hyperparameters:\n")
+#        f.write(str(sorted(best_run.items())))
+#        best_model.save("best_model.h5")
+#        print("Saved best model from hyperparameter search to disk")
+
+X,y = data()
+result = create_model(X,y)
+    
+# 
+#
+#    #RESULTS PRINTING
+#    predicted_scaled_y = best_model.predict(X_test)
+#    predicted_y = scaler.inverse_transform(predicted_scaled_y.reshape(-1,1))
+#    actual_y = scaler.inverse_transform(y_test.reshape(-1,1))
+#    mae = mean_absolute_error(actual_y, predicted_y)
+#    rmse = mean_squared_error(actual_y, predicted_y)
+#    print("Test dataset RMSE:", rmse)
+#    print("Test dataset MAE:", mae)
+    
+#    scaler, scaled_X, scaled_y = scaling()
+#    predicted_scaled_y = best_model.predict(scaled_X)
+#    predicted_y = scaler.inverse_transform(predicted_scaled_y.reshape(-1,1))
+#    actual_y = scaler.inverse_transform(scaled_y.reshape(-1,1))
+#    mae_f = mean_absolute_error(actual_y, predicted_y)
+#    rmse_f = mean_squared_error(actual_y, predicted_y)
+#    print("Full dataset RMSE:", rmse_f)
+#    print("Full dataset MAE:", mae_f)
+#    
+#    # RESULTS WRITING
+#    with open("RESULTS.txt", "w+") as f:
+#        f.write("Best performing model hyperparameters:\n")
+#        f.write(str(sorted(best_run.items())))
+#        #f.write("\n\nModel Results (scaled data):\n")
+#        #f.write("Test dataset RMSE: {}\n".format(rmse))
+#        #f.write("Test dataset MAE: {}\n".format(mae))
+#        # write model information here
+#        f.write("\n\nModel Results (units- Hartree):\n")
+#        f.write("Full dataset RMSE: {}\n".format(rmse_f))
+#        f.write("Full dataset MAE: {}\n".format(mae_f))
+#
+#    # MODEL SAVING
+#    # this model is just the last model in CV, not necessarily the best. It is the best w.r.t. hyperparameters
+#    best_model.save("best_model.h5")
+#    print("Saved best model from hyperparameter search to disk")
+    
+    
+
